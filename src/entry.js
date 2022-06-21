e421029401eeb30ede4c0b01637209a81111f7f2
zbl14 <ifthereisoneday@gmail.com>
2022-06-21 13:54:53 -0700

    create a Entry object with two properties for tile and body, add noInputtedWord function and wordCounter function
    Co-authored-by: Peter Vang <vang_peter06@yahoo.com>
    Daniel Ware <waredanielb@gmail.com>

--- src/entry.js
+++ src/entry.js
@@ -2,3 +2,31 @@ export function Entry (title, body) {
   this.title = title;
   this.body = body
 }
+
+Entry.prototype.noInputtedWord = function() {
+  for (let i=0; i < arguments.length; i++) {
+    if (arguments[i].trim().length === 0) {
+      return true;
+    }
+  }
+  return false;
+};
+
+Entry.prototype.wordCounter = function(text) {
+  if (this.noInputtedWord(text)) {
+    return 0;
+  }
+  let wordCount = 0;
+  const wordArray = text.split(" ");
+  wordArray.forEach(function(element) {
+    if (!Number(element)) {
+      wordCount++;
+    }
+  });
+  return wordCount;
+}
+
+let title = "What a wonderful day!";
+let body = "Hello, What a wonderful day!";
+let entry1 = new Entry (title, body);
+entry1.wordCounter(entry1.body)
