b7b7e42e8c9993a42a2530a3e69a9652640e8fb1
zbl14 <ifthereisoneday@gmail.com>
2022-06-21 22:50:29 -0700

    BUGFIX: vowelCounter

--- src/index.js
+++ src/index.js
@@ -1,9 +1,19 @@
 import { Entry } from './entry.js';
-import $ from 'jquery';
 import 'bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
+import $ from 'jquery';
 
 
-$(document).ready(function(){
-
+$(document).ready(function() {
+  $("form#entry").submit(function(event) {
+    event.preventDefault();
+    const title = $("#journalTitle").val();
+    const body = $("#journalBody").val();
+    let entry = new Entry(title, body);
+    let wordCount = entry.body.wordCounter();
+    let vowelCounter = entry.body.vowelCounter(entry.body);
+    let consonantCounter = entry.body.consonantCounter(entry.body);
+    $('#response').append("<li>" + wordCount + "</li>" + "<li>" + vowelCounter + "</li>" + "<li>" + consonantCounter + "</li>");
+  });
 });
